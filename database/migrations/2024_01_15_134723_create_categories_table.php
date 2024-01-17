784e8895e22e0971a8394a11bccabedddb2a314c
Manuel120903 <142826083+Manuel120903@users.noreply.github.com>
2024-01-17 10:08:48 -0600

    oa

--- database/migrations/2024_01_15_134723_create_categories_table.php
+++ database/migrations/2024_01_15_134723_create_categories_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->double('cost', 8, 2);
+            
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,16 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Here is where you can register web routes for your application.
+| These routes are loaded by the RouteServiceProvider and assigned to the "web" middleware group.
+| Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//RUTAS PARA LAS VISTAS PRINCIPALES
+Route::view('/index', 'index')->name('index'); // Redirecciona a la vista index.html
+Route::view('/tables', 'tables')->name('tables'); // Redirecciona a la vista about.html
