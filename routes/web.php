9a347e5fd7be77e86b4aadf6b82d97308507bc74
Manuel120903 <142826083+Manuel120903@users.noreply.github.com>
2024-01-22 00:24:16 -0600

    acomodo de las vistas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/booking', function () {
+    return view('booking.index');
 });
+Route::get('booking/create', [BookingController::class, 'create']);
 
 //RUTAS PARA LAS VISTAS PRINCIPALES
-Route::view('/index', 'index')->name('index'); // Redirecciona a la vista index.html
-Route::view('/tables', 'tables')->name('tables'); // Redirecciona a la vista about.html
+//Route::view('/index', 'index')->name('index'); // Redirecciona a la vista index.html
+//Route::view('/tables', 'tables')->name('tables'); // Redirecciona a la vista about.html
+//Route::view('/booking.index', 'booking')->name('booking'); // Redirecciona a la vista booking.html
